f70754e612ddf5fa9b480385380f032914545cbe
tech-camp <>
2017-12-04 18:24:36 +0900

    change styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
         # userモデルが持っているカラムをそれぞれ定義していく
         user.email = auth.info.email
         user.password = Devise.friendly_token[0,20]
-        user.name = auth.info.name
+        user.nickname = auth.info.name
         user.image = auth.info.image
         user.uid = auth.uid
         user.provider = auth.provider
